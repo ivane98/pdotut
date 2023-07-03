20762adc1fb65c5623fe024aaa3294d8130fafe7
ivane98 <i.vardoshvili22@gmail.com>
2023-07-03 14:44:36 +0400

    finished

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php 
+$dsn = 'mysql:host=localhost;dbname=world';
+
+$username = 'root';
+
+
+try {
+$db = new PDO($dsn, $username);
+}catch(PDOException $e){
+    $error_message = 'Database Error: ';
+    $error_message .= $e->getMessage();
+    echo $error_message;
+    exit();
+}
+
+?>
